9e72e2e0d575795e6404269744f63fffa2b177ed
denkyuu <denkyuu@gmail.com>
2017-05-24 22:30:10 -0400

    Allow creation of new todos.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,15 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.full_backtrace = false
+
+	config.backtrace_exclusion_patterns = [
+		/\/lib\d*\/ruby\//,
+		/bin\//,
+		/gems/,
+		/spec\/spec_helper\.rb/,
+		/lib\/rspec\/(core|expectations|matchers|mocks)/
+	]
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
