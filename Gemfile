499054e7c75849c094044f5948443c4eb6330cb8
denkyuu <denkyuu@gmail.com>
2017-05-25 12:40:42 -0400

    User now only sees their todos.

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ end
 
 group :test do
   gem 'capybara', '~> 2.14.0'
+  gem 'database_cleaner'
 end
 
 group :development do
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def new
 
   end
+
   def create
     session[:current_email] = params[:session][:email]
     redirect_to root_path
